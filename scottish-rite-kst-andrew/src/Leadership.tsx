3c2f019e3debef816f7ad1628e147d47c5f0665a
jaknyfe <delowery@gmail.com>
2026-08-11 20:37:27 +0000

    Update Treasurer Secretary card

--- scottish-rite-kst-andrew/src/Leadership.tsx
+++ scottish-rite-kst-andrew/src/Leadership.tsx
@@ -4,7 +4,7 @@ const officers = [
   { rank: 'Venerable Master', name: 'Sir Knight Joseph Felix, 32° KSA' },
   { rank: 'Senior Warden', name: 'Sir Michael Grzegorski, 32° KSA' },
   { rank: 'Junior Warden', name: 'Sir Michael Kofta, 32° KSA' },
-  { rank: 'Treasurer', name: 'Ill. Gerald Lankin, 33°' },
+  { rank: 'Treasurer/Secretary', name: 'Ill. Brian Pilz, 33° KSA' },
   { rank: 'Secretary', name: 'Bro. Joseph Felix, 32° KSA' },
   { rank: 'Chaplain', name: 'Bro. Michael Candela, 32° KSA' },
 ]
