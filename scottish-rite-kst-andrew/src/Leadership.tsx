7e82d09b3368f90fd6a69813604358b08f8c424b
jaknyfe <delowery@gmail.com>
2026-08-11 21:01:05 +0000

    Update Brian Pilz honorific

--- scottish-rite-kst-andrew/src/Leadership.tsx
+++ scottish-rite-kst-andrew/src/Leadership.tsx
@@ -4,7 +4,7 @@ const officers = [
   { rank: 'Venerable Master', name: 'Sir Knight Joseph Felix, 32° KSA' },
   { rank: 'Senior Warden', name: 'Sir Knight Michael Grzegorski, 32° KSA' },
   { rank: 'Junior Warden', name: 'Sir Knight Michael Kofta, 32° KSA' },
-  { rank: 'Treasurer/Secretary', name: 'Ill. Brian Pilz, 33° KSA' },
+  { rank: 'Treasurer/Secretary', name: 'Ill. Sir Knight Brian Pilz, 33° KSA' },
   { rank: 'Chaplain', name: 'Sir Knight Logan Gaither, 32° KSA' },
 ]
 
