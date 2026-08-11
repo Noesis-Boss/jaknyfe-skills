9d02ea303ca82117f5b305e64e515cfe60ea0a79
jaknyfe <delowery@gmail.com>
2026-08-11 20:44:16 +0000

    Remove Secretary leadership card

--- scottish-rite-kst-andrew/src/Leadership.tsx
+++ scottish-rite-kst-andrew/src/Leadership.tsx
@@ -5,7 +5,6 @@ const officers = [
   { rank: 'Senior Warden', name: 'Sir Michael Grzegorski, 32° KSA' },
   { rank: 'Junior Warden', name: 'Sir Michael Kofta, 32° KSA' },
   { rank: 'Treasurer/Secretary', name: 'Ill. Brian Pilz, 33° KSA' },
-  { rank: 'Secretary', name: 'Bro. Joseph Felix, 32° KSA' },
   { rank: 'Chaplain', name: 'Sir Knight Logan Gaither, 32° KSA' },
 ]
 
